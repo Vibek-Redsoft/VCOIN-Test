8d9022957a5acf8b938f338888704611241bc184
Vibek Dutta <vikiisbck@gmail.com>
2022-11-14 15:16:07 +0530

    cache added

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import "firebase/auth";
 import firebase from "firebase/app";
 
 const app = firebase.initializeApp({
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	apiKey: "AIzaSyBS7j4SUFdZ6KoE3AqvhFJbQ6ON_9q05Nc",
+	authDomain: "imvu-vcoin-dashboard-test.firebaseapp.com",
+	databaseURL: "https://imvu-vcoin-dashboard-test.firebaseio.com",
+	projectId: "imvu-vcoin-dashboard-test",
+	storageBucket: "imvu-vcoin-dashboard-test.appspot.com",
+	messagingSenderId: "542956967549",
+	appId: "1:542956967549:web:e9cdf6641505b2c76c8314",
+	measurementId: "G-6ZN8RK09TW",
 });
 
 export const auth = app.auth();
